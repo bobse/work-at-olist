5aff320ead6e15d9de586315078beb714fd9cafe
Roberto Seba <roberto@robertoseba.com>
2022-07-15 16:20:38 -0300

    docker and dockercompose to run everything

--- library/library/settings/development.py
+++ library/library/settings/development.py
@@ -9,7 +9,7 @@ Django development settings for library project.
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INTERNAL_IPS = [
@@ -18,5 +18,9 @@ INTERNAL_IPS = [
 # django toolbar
 MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 INSTALLED_APPS.append('debug_toolbar')
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG,
+}
 
 DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600, ssl_require=False)}
